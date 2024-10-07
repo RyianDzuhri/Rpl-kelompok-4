16ad62441408ead4b794e51246d4b0cafaa15ab8
RyianDzuhri <muhammadryiandzuhri@gmail.com>
2024-10-07 15:17:27 +0800

    Saya belajar Mengetik

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> 
+    {{-- halo --}}
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aplikasi Manajemen Barang</title>
