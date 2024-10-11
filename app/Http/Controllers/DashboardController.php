1bdc2cad679a7fae821bf661f204d92a130ca951
RyianDzuhri <muhammadryiandzuhri@gmail.com>
2024-10-11 14:38:26 +0800

    Tambah bacjground dan controller baru

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Barang;
+use App\Models\Kategori;
+use App\Models\Pemasok;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        // Menghitung jumlah barang
+        $jumlah_jenis_barang = Barang::distinct('nama_barang')->count();
+
+        // Menghitung jumlah pemasok
+        $jumlah_pemasok = Pemasok::count();
+
+        // Menghitung jumlah kategori
+        $jumlah_kategori = Kategori::count();
+
+        // Mengirim data ke view
+        return view('barang.dashboard', compact(
+            'jumlah_jenis_barang',
+            'jumlah_pemasok',
+            'jumlah_kategori',
+        ));
+    }
+}
